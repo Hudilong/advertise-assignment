70eda9d5f4ec936131c62e2b88808e06713cbe40
Ian Hudicourt <ihudicourt@gmail.com>
2024-01-31 14:02:22 -0500

    added custom query to ListingRepository

--- src/main/java/com/hudilong/advertassignment/persistence/repositories/ListingRepository.java
+++ src/main/java/com/hudilong/advertassignment/persistence/repositories/ListingRepository.java
@@ -1,11 +1,19 @@
 package com.hudilong.advertassignment.persistence.repositories;
 
 import com.hudilong.advertassignment.domain.entities.ListingEntity;
+import com.hudilong.advertassignment.domain.enums.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ListingRepository extends JpaRepository<ListingEntity, UUID> {
+
+    @Query(value = "SELECT * FROM listings WHERE dealer_id = :dealerId AND state = :state", nativeQuery = true)
+    List<ListingEntity> findAllListingsByDealerAndState(@Param("dealerId") UUID dealerId, @Param("state") State state);
 }
