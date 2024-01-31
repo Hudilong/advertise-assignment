38df7370af13f0ba917051596fa1c1d4fa4bc7ed
Ian Hudicourt <ihudicourt@gmail.com>
2024-01-30 21:35:35 -0500

    project init

---
+++ src/main/java/com/hudilong/advertassignment/AdvertAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.hudilong.advertassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hudilong/advertassignment/AdvertAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudilong.advertassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
