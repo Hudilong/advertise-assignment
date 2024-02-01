21c172081973bae157aa939f1055d7fdf68728c6
Ian Hudicourt <ihudicourt@gmail.com>
2024-02-01 14:19:37 -0500

    added SwaggerConfiguration

---
+++ src/main/java/com/hudilong/advertassignment/config/SwaggerConfiguration.java
@@ -0,0 +1,19 @@
+package com.hudilong.advertassignment.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("AdvertisementApi")
+                .version("1.0")
+                .description("API to manage dealers and listings"));
+    }
+}
