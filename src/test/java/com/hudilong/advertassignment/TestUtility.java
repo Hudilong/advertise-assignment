32a3a9c76af850512c57669397f282d401752dcc
Ian Hudicourt <ihudicourt@gmail.com>
2024-02-03 12:45:14 -0500

    removed id from test dealers

--- src/test/java/com/hudilong/advertassignment/TestUtility.java
+++ src/test/java/com/hudilong/advertassignment/TestUtility.java
@@ -12,13 +12,15 @@ public class TestUtility {
 
     public static DealerDto createDealerDto1() {
         return new DealerDto(
-                 UUID.fromString("713c1c58-cc4f-45d2-a398-90cf988b51b2"),
+//                 UUID.fromString("713c1c58-cc4f-45d2-a398-90cf988b51b2"),
+                null,
                 "Crimson Cars");
     }
 
     public static ListingDto createListingDto1() {
         return new ListingDto(
-                UUID.fromString("df98776f-25f0-4e63-a1a6-08f68e6e3d25"),
+//                UUID.fromString("df98776f-25f0-4e63-a1a6-08f68e6e3d25"),
+                null,
                 createDealerDto1(),
                 "Mazda 3",
                 BigDecimal.valueOf(7000.00),
@@ -28,7 +30,8 @@ public class TestUtility {
 
     public static ListingDto createListingDto2() {
         return new ListingDto(
-                UUID.fromString("f2383681-f8b8-4e0c-a8dd-61406448f7c8"),
+//                UUID.fromString("f2383681-f8b8-4e0c-a8dd-61406448f7c8"),
+                null,
                 createDealerDto1(),
                 "Honda Civic",
                 BigDecimal.valueOf(3000.00),
@@ -38,7 +41,8 @@ public class TestUtility {
 
     public static ListingDto createListingDto3() {
         return new ListingDto(
-                UUID.fromString("0d45cc2f-8df4-4dd3-91b0-68ee4a73c67c"),
+//                UUID.fromString("0d45cc2f-8df4-4dd3-91b0-68ee4a73c67c"),
+                null,
                 createDealerDto1(),
                 "Jeep Wrangler",
                 BigDecimal.valueOf(23000.00),
